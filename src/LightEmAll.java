b8d6863a535dbd0f7933246a188e59ce77bcd807
Sauharda Rajbhandari <sauharda19@gmail.com>
2019-04-03 16:46:50 -0400

    Initial commit

---
+++ src/LightEmAll.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+import tester.*;
+import javalib.impworld.*;
+import java.awt.Color;
+import javalib.worldimages.*;
+
+class LightEmAll extends World {
+  // a list of columns of GamePieces,
+  // i.e., represents the board in column-major order
+  ArrayList<ArrayList<GamePiece>> board;
+  // a list of all nodes
+  ArrayList<GamePiece> nodes;
+  // a list of edges of the minimum spanning tree
+  ArrayList<Edge> mst;
+  // the width and height of the board
+  int width;
+  int height;
+  // the current location of the power station,
+  // as well as its effective radius
+  int powerRow;
+  int powerCol;
+  int radius;
+  
+  public WorldScene makeScene() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+}
+
+class GamePiece {
+  // in logical coordinates, with the origin
+  // at the top-left corner of the screen
+  int row;
+  int col;
+  // whether this GamePiece is connected to the
+  // adjacent left, right, top, or bottom pieces
+  boolean left;
+  boolean right;
+  boolean top;
+  boolean bottom;
+  // whether the power station is on this piece
+  boolean powerStation;
+}
+
+class Edge {
+  GamePiece fromNode;
+  GamePiece toNode;
+  int weight;
+}
+
